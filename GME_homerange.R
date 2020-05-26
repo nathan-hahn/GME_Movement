28b0097d281c336a786562a733656d4e88500b64
nathan-hahn <nhahnwa@gmail.com>
2020-05-26 14:52:34 -0600

    Running on all GME elephants

--- GME_homerange.R
+++ GME_homerange.R
@@ -7,13 +7,9 @@ gme <- readRDS("./movdata/GMEcollars_002_clean_2020-05-21.rds")
 
 # prep spdf
 t <-  filter(gme, !is.na(x)) %>%
-  dplyr::select(x, y, subject_name) %>%
-  filter(subject_name == c("Ivy", "Fred")) 
+  dplyr::select(x, y, subject_name)
+  #filter(subject_name %in% c("Lowana", "Fred", "Olchoda")) 
 coordinates(t) <-~x+y
-
-# build MCP's 
-mcp <- mcp(t[,1], percent = 100)
-plot(mcp)
 
 # build MCP's by subject_name
 split <- split(t, t$subject_name)
@@ -23,30 +19,17 @@ mcp <- lapply(split, mcp)
 ##### cut raster stack by 
 s <- readRDS("./spatial data/GME_rasterStack_20200522.rds")
 
-# mcp.sf <- st_as_sf(t)
-# st_crs(mcp.sf) = 32736
-
-# clip rasters to homeranges
-cr <- crop(s, extent(mcp), snap="out")                    
-fr <- rasterize(mcp, cr)   
-lr <- mask(x=cr, mask=fr)
-
-
-
 mask.poly.raster <- function(poly, raster) {
   # crop raster to polygon extent
   cr <- crop(raster, extent(poly), snap = "out")
-  # # rasterize the polygon for masking
+  # rasterize the polygon for masking
   fr <- rasterize(poly, cr)
-  # # mask the 
+  # mask the raster by mcp 
   lr <- mask(x = cr, mask = fr)
   return(lr)
 }
 
-
 system.time(mcp.rast <- lapply(mcp, mask.poly.raster, raster = s$change03_181_reclassMara_2019.11.22))
-
-
 
 
 # landscape metrics
