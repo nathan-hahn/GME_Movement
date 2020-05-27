9fb2624be9e51c0dd697e1ce10b7a9c341c057bb
nathan-hahn <nhahnwa@gmail.com>
2020-05-28 00:17:59 +0300

    mcp mask: remove rasterization to speed up processing

--- GME_homerange.R
+++ GME_homerange.R
@@ -8,13 +8,12 @@ gme <- readRDS("./movdata/GMEcollars_002_clean_2020-05-21.rds")
 # prep spdf
 t <-  filter(gme, !is.na(x)) %>%
   dplyr::select(x, y, subject_name) %>%
-  filter(subject_name %in% c("Lowana", "Fred", "Olchoda")) 
+  filter(subject_name %in% c("Tressa", "Fred", "Olchoda")) 
 coordinates(t) <-~x+y
 
 # build MCP's by subject_name
 split <- split(t, t$subject_name)
 mcp <- lapply(split, mcp)
-
 
 ##### cut raster stack by 
 s <- readRDS("./spatial data/GME_rasterStack_20200522.rds")
@@ -22,15 +21,17 @@ s <- readRDS("./spatial data/GME_rasterStack_20200522.rds")
 mask.poly.raster <- function(poly, raster) {
   # crop raster to polygon extent
   cr <- crop(raster, extent(poly), snap = "out")
-  # rasterize the polygon for masking
-  fr <- rasterize(poly, cr)
   # mask the raster by mcp 
-  lr <- mask(x = cr, mask = fr)
+  lr <- mask(x = cr, mask = poly)
   return(lr)
 }
 
-system.time(mcp.rast <- lapply(mcp, mask.poly.raster, raster = s$change03_181_reclassMara_2019.11.22))
+# mask landcover raster to homeranges
+system.time(
+  mcp.rast <- lapply(mcp, mask.poly.raster, raster = s$change03_181_reclassMara_2019.11.22)
+)
 
+saveRDS(mcp.rast, paste0("./homerange/GME_mcp_rasters_002", Sys.Date(),".rds"))
 
 # landscape metrics
 library(landscapemetrics)
@@ -40,4 +41,5 @@ metrics <- lsm_l_ed(mcp.rast$Olchoda)
 
 metrics <- lapply(mcp.rast, lsm_l_ed)
 
+metrics[[1]]$value
 
