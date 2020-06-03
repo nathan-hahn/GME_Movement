6e504f8c2a1fde6f9f2c904122c4f87b638a1b9c
nathan-hahn <nhahnwa@gmail.com>
2020-06-03 13:04:27 -0600

    Add filter to bursts before witholding

--- HMM_prepData_pop_GME.R
+++ HMM_prepData_pop_GME.R
@@ -2,12 +2,12 @@
 library(dplyr)
 library(momentuHMM)
 
-source(velocity.R)
+source("~/Dropbox (Personal)/R/Functions/velocity.R")
 
 #' ---- Prep data for HMM population model fitting ----
 
 # Add data with raster covariates. 
-used.all <- readRDS("./movdata/GMEcollars_001_usedClust_2020-01-14.rds")
+used.all <- readRDS("./GMM/GMEcollars_002_usedClust_2020-06-02.rds")
 used.all <- used.all[!is.na(used.all$x),]
 
 # standardize covariates
@@ -22,7 +22,7 @@ dim(st.covs)
 
 # merge with reloc data
 used.st <- used.all %>%
-  dplyr::select(-cor.vars) %>%
+  dplyr::select(-all_of(cor.vars)) %>%
   bind_cols(., st.covs) %>%
   rename(ID = burst) %>% #for HMM package
   droplevels()
@@ -30,33 +30,44 @@ used.st <- used.all %>%
 
 # must be a dataframe to work!! 
 used.st <- as.data.frame(used.st)
-saveRDS(used.st, "./movdata/GMEcollars_001_usedClustST_2020-05-14.rds")
+saveRDS(used.st, "./HMM/GMEcollars_001_usedClustST_2020-05-14.rds")
 
 
 #' ---- Create Test/Train Sets ----
 #' Split the data into testing and training sets
 #' Last n% of each burst for the testing, defined by 'cut'
 
-split <- split(used.st, used.st$ID)
-split <- c(split[1], split[3]) #TEMP -- only use Caroline and Kegol
 
-f <- function(x, cut, type) {
+# filter to bursts that are large enough for test/train
+t <- used.st %>%
+  group_by(ID, subject_name) %>% tally() %>%
+  mutate(train = n*.90, test = n*.10) %>%
+  filter(train > 500 & test > 500) %>% droplevels()
+
+used.st <- filter(used.st, ID %in% t$ID) %>% droplevels()
+
+# divide datasets by individual
+split <- split(used.st, used.st$ID)
+#split <- c(split[1], split[3]) #TEMP -- only use Caroline and Kegol
+
+withold <- function(x, cut, type) {
   n <- nrow(x)*cut
   
   if (type == "train") {
     y <- head(x, (nrow(x)-n))
   }
   
-  if (type == "test")
+  if (type == "test") {
     y <- tail(x, n)
+  }
   
   return(y)
 }
 
 cut = .10
-train <- lapply(split, f, cut = cut, type = "train")
+train <- lapply(split, withold, cut = cut, type = "train")
 train <- do.call("rbind", train)
-test <- lapply(split, f, cut = cut, type = "test")
+test <- lapply(split, withold, cut = cut, type = "test")
 test <- do.call("rbind", test)
 
 #' ---- create momentu objects ----
@@ -71,6 +82,4 @@ ele.log.step <- ele.step
 ele.log.step$step <- log(ele.log.step$step + 0.001) # add constant for zero steps
 
 # save as rdata for us on secondary machines
-saveRDS(ele.log.step, ".HMM/eleTrain_logStep_mmt_TEST.rds")
-
-
+saveRDS(ele.log.step, ".HMM/GMEcollars_001_logStep_train_TEST.rds")
