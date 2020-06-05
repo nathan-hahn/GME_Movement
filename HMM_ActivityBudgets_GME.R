f6be3fddfd79b565bc9dad0145814c9305a783d1
nathan-hahn <nhahnwa@gmail.com>
2020-06-05 09:42:35 -0600

    Fix budget plot function

--- HMM_ActivityBudgets_GME.R
+++ HMM_ActivityBudgets_GME.R
@@ -9,18 +9,6 @@ library(tidyverse)
 library(xts)
 library(zoo)
 
-
-hmm.df <- readRDS("./HMM/TEST_m1_indiv_df.rds")
-output <- readRDS("./HMM/TEST_traindf_original.rds")
-
-output$viterbi <- hmm.df$viterbi
-output$ag.used <- ifelse(output$lc.estes == 1, 1, 0)
-output$pa <- factor(output$pa, levels = c(3:1))
-output$pa.2 <- ifelse(output$pa == 1 & output$ag.used == 1, 4, output$pa)
-
-
-###### Identify Ag Use Windows #####
-
 # function to return stats from roll apply
 rollstat <- function(x, na.rm = TRUE) {
   # x     = numeric vector
@@ -31,13 +19,28 @@ rollstat <- function(x, na.rm = TRUE) {
   hi <- m + 2*s
   lo <- m - 2*s
   max <- max(x) # all points within the window will be labeled 1 - signals a "raiding period" for activity budgets/plotting
-
+  
   ret <- c(mean = m, stdev = s, hi.95 = hi, lo.95 = lo, ag.window = max) 
   return(ret)
 }
 
+
+##### Prep Data #####
+hmm.df <- readRDS("./HMM/TEST_m1_indiv_df.rds")
+output <- readRDS("./HMM/TEST_traindf_original.rds")
+
+# add viterbi estimates to the original dataframe -- revert to non-standardized covs and steps
+output$viterbi <- hmm.df$viterbi
+# create new varibles to differentiate ag and non-ag in non-protected areas
+output$ag.used <- ifelse(output$lc.estes == 1, 1, 0)
+output$pa <- factor(output$pa, levels = c(3:1))
+output$pa.2 <- ifelse(output$pa == 1 & output$ag.used == 1, 4, output$pa)
+
+
+###### Identify Ag Use Windows #####
+
 # Apply rollstat. Create time series and apply rollstat function
-window <- 25 # in hours. Split to before and after when align = center
+window <- 49 # in hours. Split to before and after when align = center
 split <- split(output, output$burst) # careful of what you split by. May cuase problems splitting by name if there are large gaps between recollars. Let na.rm = FALSE in rollstat
 
 system.time({
@@ -62,7 +65,7 @@ t <- output %>%
 # output$np <- ifelse(output$pa == "not protected", 1, 0) # calculate ag.used (0,1)
 # output$lu <- ifelse(output$pa == "limited use", 1, 0) # calculate ag.used (0,1)
 
-output <- mutate(output, viterbi = recode_factor(viterbi, 
+output.plot <- mutate(output, viterbi = recode_factor(viterbi, 
                                                  "1" = "encamped",
                                                  "2" = "foraging",
                                                  "3" = "transit"),
@@ -72,9 +75,8 @@ output <- mutate(output, viterbi = recode_factor(viterbi,
 
 
 # fix NAs for the ag window
-t <- ifelse(is.na(output$ag.window) == TRUE, 0, output$ag.window)
-output$ag.window <- as.factor(t)
-
+t <- ifelse(is.na(output.plot$ag.window) == TRUE, 0, output.plot$ag.window)
+output.plot$ag.window <- as.factor(t)
 
 ## Density plots v2
 theme_set(  theme_bw() + # set theme with no legend of strip text
@@ -87,11 +89,12 @@ theme_set(  theme_bw() + # set theme with no legend of strip text
 )
 
 
+# function for plotting activity budgets
 plot_budget <- function(t=t, facet, title){
   ggplot(t, aes(hour(date))) +
     facet_grid(facet) +
     geom_density(aes(x = hour(date),
-                     fill = factor(viterbi), colour = factor(viterbi)), alpha = 0.3, adjust = 1.5)
+                     fill = factor(viterbi), colour = factor(viterbi)), alpha = 0.3, adjust = 1.5) +
   
   # add day/night lines - code for shading below
   geom_vline(aes(xintercept=6),
@@ -106,10 +109,10 @@ plot_budget <- function(t=t, facet, title){
 }
 
 # budget by ag window
-plot_budget(t = output, facet = viterbi ~ as.factor(max), title = "ag.class.mean")
+plot_budget(t = output.plot, facet = viterbi ~ ag.window, title = "GME: Activity budget by ag window")
 
 # budget by land use
-plot_budget(t = output, facet = viterbi ~ pa.2, title = "GME: Activity budget by land use type")
+plot_budget(t = output.plot, facet = viterbi ~ pa.2, title = "GME: Activity budget by land use type")
 
 
 
