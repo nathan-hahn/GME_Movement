ff10f9708635f02f50787e25b785351d53b0031d
nathan-hahn <nhahnwa@gmail.com>
2020-06-23 22:17:40 +0300

    Pre-clustering filter to downsample fixes and remove collars outside study area

---
+++ movdata_temporalfilter.R
@@ -0,0 +1,29 @@
+#### Used Filter #####
+
+movdat <- readRDS('./movdata/GMEcollars_002_used_2020-06-20.rds')
+movdat$subject_name <- ifelse(is.na(movdat$subject_name), movdat$collar_id, movdat$subject_name)
+
+gr <- movdat %>%
+  filter(site == "gr") %>%
+  filter(!(subject_name %in% c("Nyanza"))) %>%
+  # filter(minute(date) <= 10 | minute(date) >= 50) %>%
+  droplevels()
+
+mep <- movdat %>%
+  filter(site == "mep") %>%
+  filter(!(subject_name %in% c("Tunai","Bettye","Nancy", "Wilbur", 
+                               "Julia", "Ndorre", "Nkoidila", "Santiyan", "Earhart",
+                               "Rudisha", "Naeku", "Olkeri", "ST2010-1441"))) 
+
+
+movdat.filter <- rbind(gr, mep) %>%
+  filter(minute(date) <= 10 | minute(date) >= 50)
+
+fixes <- movdat.filter %>%
+  group_by(subject_name) %>%
+  tally() %>%
+  filter(n >= 120*24)
+
+movdat.filter <- filter(movdat.filter, subject_name %in% fixes$subject_name)
+
+saveRDS(movdat.filter, "./movdata/GMEcollars_002_usedFilter_2020-06-20.rds")
