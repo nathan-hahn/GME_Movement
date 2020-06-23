5b9acaaa83da79474146ca1f36a85e2720b1dd77
nathan-hahn <nhahnwa@gmail.com>
2020-06-23 22:19:10 +0300

    Time series analysis of resource use

---
+++ GMM/TemporalUse_GME.R
@@ -0,0 +1,46 @@
+##### Ag Temporal Use #####
+library(tidyverse)
+source("GME_functions.R")
+
+movdat <- readRDS('./GMM/movdata/GMEcollars_002_usedClust_2020-06-05.rds')
+movdat$ag.used <- ifelse(movdat$lc.estes == 1, 1, 0) 
+
+movdat <- filter(movdat, subject_name %in% c('Ivy', 'Kegol', 'Lowana', 'Olchoda', 'Jacinta')) %>% droplevels()
+
+##### Rolling Stats #####
+split <- split(movdat, movdat$subject_name)
+window <- c(1*24, 7*24, 30*24, 90*24) # in hours. Split to before and after when align = center
+
+output.plot <- NULL
+for(i in 1:length(window)){
+  # calculate moving window stats - see rollstats function
+  output.plot[[i]] <- window_stats(df.list = split, window = window[[i]])
+  # remove NAs from ag windows - created by window cutoff
+  output.plot[[i]] <- filter(output.plot[[i]], is.na(ag.window) == FALSE) %>% droplevels()
+}
+
+##### Visualize #####
+res.90 <- output.plot[[4]] 
+res.90$ag.used <- ifelse(res.90$ag.used == 1, 0.75, NA) # adjust ag.used values for plotting for visibility
+p90 <- ggplot(res.90, aes(x = date, color = subject_name)) + #, color = name
+  geom_point(aes(y = ag.used), color = "grey40", size = .2, alpha = 0.5) +
+  geom_ribbon(aes(ymin = lo.95, ymax = hi.95), size = .5, alpha = 0.4) +
+  geom_line(aes(y = mean), size = 1, alpha = 0.7) +
+  facet_wrap(~ subject_name) +
+  labs(title = "Ag Usage: Volatility and Trend",
+       subtitle = "60-Day Moving Average with 95% CI Bands") +
+  theme(legend.position="none")
+
+p90
+
+
+
+
+roll.max <- output.plot[[2]] %>%
+  group_by(subject_name) %>%
+  summarise(roll.max = max(mean))
+
+roll.max.year <- roll.max %>%
+  group_by(subject_name) %>%
+  summarise(roll.mean = mean(roll.max))
+
