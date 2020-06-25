01733c85191713ae3401175e417ba3196ddbdda0
nathan-hahn <nhahnwa@gmail.com>
2020-06-25 22:58:37 +0300

    Filter to screen individuals by data length before generating windows

--- GMM/TemporalUse_GME.R
+++ GMM/TemporalUse_GME.R
@@ -3,13 +3,19 @@ library(tidyverse)
 source("GME_functions.R")
 
 ##### Data Prep #####
-movdat <- readRDS('./movdata/GMEcollars_002_usedFilter_2020-06-20.rds')
-movdat$ag.used <- ifelse(movdat$lc.estes == 1, 1, 0) 
+movdat.filter <- readRDS('./movdata/GMEcollars_002_usedFilter_2020-06-20.rds')
+movdat.filter$ag.used <- ifelse(movdat.filter$lc.estes == 1, 1, 0) 
 
 ##### Rolling Stats #####
-split <- split(movdat, movdat$subject_name)
+roll.filter <- movdat.filter %>%
+  group_by(subject_name) %>%
+  tally() %>% 
+  filter(n >= 2880) 
 
-window <- c(1*24, 7*24, 30*24, 90*24) # in hours. Split to before and after when align = center
+roll.df <- filter(df, subject_name %in% roll.filter$subject_name)
+split <- split(roll.df, roll.df$subject_name)
+
+window <- c(7*24, 30*24, 60*24, 90*24) # in hours. Split to before and after when align = center
 
 output.plot <- NULL
 for(i in 1:length(window)){
@@ -20,7 +26,8 @@ for(i in 1:length(window)){
 }
 
 ##### Visualize #####
-res.90 <- output.plot[[3]] 
+res.90 <- output.plot[[4]] %>%
+  filter(year(date) >= 2018)
 res.90$ag.used <- ifelse(res.90$ag.used == 1, 0.75, NA) # adjust ag.used values for plotting for visibility
 p90 <- ggplot(res.90, aes(x = date, color = subject_name)) + #, color = name
   geom_point(aes(y = ag.used), color = "grey40", size = .2, alpha = 0.5) +
@@ -28,7 +35,7 @@ p90 <- ggplot(res.90, aes(x = date, color = subject_name)) + #, color = name
   geom_line(aes(y = mean), size = 1, alpha = 0.7) +
   facet_wrap(~ subject_name) +
   labs(title = "Ag Usage: Volatility and Trend",
-       subtitle = "60-Day Moving Average with 95% CI Bands") +
+       subtitle = "90-Day Moving Average with 95% CI Bands") +
   theme(legend.position="none")
 
 p90
