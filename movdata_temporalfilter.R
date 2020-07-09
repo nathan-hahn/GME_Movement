f99e88ebac1f0d6781bc6baacaedb5039277e547
nathan-hahn <nhahnwa@gmail.com>
2020-07-09 09:30:06 -0600

    Downsamples collars to 1hr fixes and removes individuals outside study area

--- movdata_temporalfilter.R
+++ movdata_temporalfilter.R
@@ -8,9 +8,7 @@ movdat$subject_name <- ifelse(is.na(movdat$subject_name), movdat$collar_id, movd
 
 gr <- movdat %>%
   filter(site == "gr") %>%
-  # mutate(collar_id = tolower(collar_id)) %>%
-  # mutate(subject_name = collar_id) %>% ## TEMPORARY
-  filter(!(subject_name %in% c("Nyanza"))) %>% #"st2010-3047"
+  filter(!(subject_name %in% c("Nyanza"))) %>%
   filter(minute(date) <= 10 | minute(date) >= 50) %>%
   droplevels()
 
@@ -20,41 +18,6 @@ mep <- movdat %>%
                                "Julia", "Ndorre", "Nkoidila", "Santiyan", "Earhart",
                                "Rudisha", "Naeku", "Olkeri", "ST2010-1441"))) 
 
-
 downsampled <- rbind(gr, mep) 
 
-fixes <- downsampled %>%
-  group_by(subject_name) %>%
-  tally() %>%
-  filter(n >= 120*24)
-
-movdat.filter <- filter(downsampled, subject_name %in% fixes$subject_name)
-
-saveRDS(movdat.filter, "./movdata/GMEcollars_002_usedFilter_2020-06-30.rds")
-
-
-### TEMPORARY
-
-# gr <- movdat %>%
-#   filter(site == "gr") %>%
-#   mutate(collar_id = tolower(collar_id)) %>%
-#   mutate(subject_name = collar_id) %>% ## TEMPORARY
-#   filter(!(subject_name %in% c("st2010-3047"))) %>% #"st2010-3042"
-#   filter(minute(date) <= 10 | minute(date) >= 50) %>%
-#   droplevels()
-# 
-# mep <- movdat %>%
-#   filter(site == "mep") %>%
-#   filter(!(subject_name %in% c("Tunai","Bettye","Nancy", "Wilbur", 
-#                                "Julia", "Ndorre", "Nkoidila", "Santiyan", "Earhart",
-#                                "Rudisha", "Naeku", "Olkeri", "ST2010-1441"))) 
-# 
-# 
-# downsampled <- rbind(gr, mep) 
-# 
-# fixes <- downsampled %>%
-#   group_by(subject_name) %>%
-#   tally() %>%
-#   filter(n >= 120*24)
-# 
-# temp.filter <- filter(downsampled, subject_name %in% fixes$subject_name)
+saveRDS(downsampled, "./movdata/GMEcollars_002_usedFilter_2020-06-30.rds")
