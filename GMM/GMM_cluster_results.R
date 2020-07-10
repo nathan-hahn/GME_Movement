0e1c5d97d2d55ae851c8191ea4cc187603532508
nathan-hahn <nhahnwa@gmail.com>
2020-07-10 11:09:15 -0600

    update usedClust save

--- GMM/GMM_cluster_results.R
+++ GMM/GMM_cluster_results.R
@@ -1,23 +1,28 @@
 ##### GMM Model Visualization #####
 # For visualizing and compiling GMM results. 
 # Attach classifications to the dataframe
+library(tidyverse)
+library(lubridate)
+library(mclust)
+
 
 # Load cluster results table
 
-movdat <- readRDS('./GMM/res90_agmovingwindow.rds') 
+movdat <- readRDS('./GMM/GMEcollars_002_res90_2020-07-09.rds') %>%
+  mutate(subject_sex = if_else(subject_sex %in% c("male", "M"), "male", "female"))
   
 mod <- readRDS('./GMM/results/modSelect.rds')
 
-clust.result <- read.csv("./GMM/results/clustResult_GME_2020-06-25.csv")
+clust.result <- read.csv("./GMM/results/clustResult_GME_2020-07-09.csv")
 clust.summary <- clust.result %>%
-  group_by(ag.class.roll) %>%
-  summarise(n = length(ag.class.roll),
+  group_by(ag.class.mean) %>%
+  summarise(n = length(ag.class.mean),
             mean = mean(mean.occupancy),
             sd = sd(mean.occupancy),
             se = sd/sqrt(n),
             lower.ci = mean - qt(1 - (0.05 / 2), n - 1) * se,
             upper.ci = mean + qt(1 - (0.05 / 2), n - 1) * se)
-(clust.summary)
+(clust.summary) # overall
 
 
 ##### Predict tactics for each season #####
@@ -29,7 +34,7 @@ class <- list()
 for(i in 1:length(split)) {
   ag.mean <- split[[i]] %>%
     mutate(year.cuts = as.character(year.cuts)) %>%
-    group_by(subject_name, subject_sex, year.cuts, cut.date) %>%
+    group_by(subject_name, subject_sex, subject_age, site, region, year.cuts, cut.date) %>%
     summarise(n = n(),
               mean.occupancy = mean(ag.used))
   mod.predict[[i]] <- predict.Mclust(mod, ag.mean$mean.occupancy)
@@ -55,15 +60,10 @@ for(i in 1:length(mod.predict)){
 }
 
 
-# TODO: bind seasonal and overall results to movdata
-t <- do.call(rbind, result) %>%
-  select(subject_name, year.cuts, ag.class.season) %>%
-  
-
-
+##### Frequency Tables #####
 
 ## clust summary - by individual season
-tactics.season <- do.call(rbind, result) %>%
+tactics.summary <- do.call(rbind, result) %>%
   group_by(ag.class.season) %>%
   summarise(n = n(),
             mean = mean(mean.occupancy),
@@ -71,8 +71,16 @@ tactics.season <- do.call(rbind, result) %>%
             se = sd/sqrt(n),
             c.05 = mean - qt(1 - (0.05 / 2), n - 1) * se,
             c.95 = mean + qt(1 - (0.05 / 2), n - 1) * se)
-tactics.season 
+tactics.summary 
 
+## individual season classifications
+tactics.seasons <- do.call(rbind, result) %>%
+  group_by(ag.class.season) %>%
+  select(subject_name, subject_sex, site, region, year.cuts, ag.class.season) %>%
+  spread(key = year.cuts, value = ag.class.season) %>%
+  mutate(mean.occupancy = clust.result$mean.occupancy, ag.class = clust.result$ag.class.mean) %>%
+  relocate(subject_name, subject_sex, region, site, mean.occupancy, ag.class)
+tactics.seasons
 
 ## tactic summary over time with rel freqs
 season <- do.call(rbind, result) %>%
@@ -86,13 +94,48 @@ ggplot(data = season, aes(x = cut.date, y = prop)) +
   geom_line(aes(color = as.factor(ag.class.season), linetype = as.factor(ag.class.season))) + 
   geom_point(aes(color = as.factor(ag.class.season)))
 
-
 ## tactics by sex
 tactics.sex <- do.call(rbind, result) %>%
-  group_by(ag.class.season, subject_sex) %>%
-  summarise(n = n())
+  group_by(subject_sex, ag.class.season) %>%
+  summarise(n = n()) %>%
+  mutate(prop=round(n/sum(n), 2))
+tactics.sex
 
 
-## tactics by site
+## tactics by site - frequency calculated relative to the site
+tactics.region <- do.call(rbind, result) %>%
+  group_by(region, ag.class.season) %>%
+  summarise(n = n()) %>%
+  mutate(prop=round(n/sum(n), 2))
+tactics.region
+
+tactics.site <- do.call(rbind, result) %>%
+  group_by(site, ag.class.season) %>%
+  summarise(n = n()) %>%
+  mutate(prop=round(n/sum(n), 2))
+tactics.site
+
+##### Add seasonal and aggregated results to the dataframe
+
+# bind lumped results to movdata
+agg.clust <- clust.result %>%
+  select(subject_name, tactic.agg = ag.class.mean) %>%
+  inner_join(., movdat, by = c("subject_name")) 
+
+# bind summarized results
+clust.df <- do.call(rbind, result) %>%
+  ungroup() %>%
+  select(subject_name, year.cuts, tactic.season = ag.class.season) %>%
+  inner_join(., agg.clust, by = c("subject_name", "year.cuts")) %>%
+  as.data.frame()
+
+
+##### Save Tables #####
+# write.csv(tactics.summary, './GMM/results/cluster tables/tactics_summary.csv')
+# write.csv(tactics.seasons, './GMM/results/cluster tables/tactics_season_classes.csv')
+# write.csv(tactics.sex, './GMM/results/cluster tables/tactics_sex.csv')
+#write.csv(tactics.site, './GMM/results/cluster tables/tactics_site.csv')
+# write.csv(tactics.region, './GMM/results/cluster tables/tactics_region.csv')
+saveRDS(clust.df, './GMM/GMEcollars_002_usedClust_2020-07-09.rds')
 
 
