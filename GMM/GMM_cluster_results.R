f9a5a92eabaa4341a6380f8867db3a35df6cc0b0
nathan-hahn <nhahnwa@gmail.com>
2020-11-18 11:26:26 -0700

    code cleanup

--- GMM/GMM_cluster_results.R
+++ GMM/GMM_cluster_results.R
@@ -18,11 +18,11 @@ source('GME_functions.R')
 
 # Load cluster results table
 
-movdat <- readRDS('./GMM/GMEcollars_002_res90_2020-07-14.rds') 
+movdat <- readRDS('./GMM/movdata/GMEcollars_003_res90_2020-10-30.rds') 
   
-mod <- readRDS('./GMM/results/mSelect_2020-07-14.rds')
+mod <- readRDS('./GMM/results/mSelect_2020-10-30.rds')
 
-clust.result <- read.csv("./GMM/results/clustResult_GME_2020-07-14.csv")
+clust.result <- read.csv("./GMM/results/clustResult_GME_2020-10-30.csv") 
 clust.summary <- clust.result %>%
   mutate(ag.class.mean = recode_factor(as.factor(ag.class.mean),
                                        "1" = "Rare",
@@ -36,6 +36,7 @@ clust.summary <- clust.result %>%
             upr.ci = MeanCI(mean.occupancy, method = "boot", type = "norm", R=200)[3]) 
 (clust.summary) # overall
 
+## Plot cluster summaries by mean ag use trends
 ggplot(clust.summary, aes(ag.class.mean, mean)) + geom_pointrange(
   aes(ymin = lwr.ci, ymax = upr.ci, color = ag.class.mean), 
   position = position_dodge(0.3)) + 
@@ -47,7 +48,6 @@ ggplot(clust.summary, aes(ag.class.mean, mean)) + geom_boxplot(
   position = position_dodge(0.3)) + 
   scale_color_manual(values = c("blue", "#FF0000FF", "#80FF00FF", "#8000FFFF")) +
   xlab("Tactic Class") + ylab("Mean Occupancy") + labs(color = "Tactic Class")
-
 
 
 ##### Get cut points for the ag classes #####
@@ -74,14 +74,10 @@ for(i in 1:length(split)) {
   mod.predict[[i]] <- predict.Mclust(mod, ag.mean$mean.occupancy)
   
   # create result table for each season year
-  
   result[[i]] <- ag.mean
-  #result[[i]][,paste0("ag.class.",ag.mean$year.cuts[1])] <- mod.predict[[i]]$classification
   result[[i]]$ag.class.year <- mod.predict[[i]]$classification
 }
 
-
-#names(result) <- c("2011", '2012', "2013", "2014", "2015", "2016", "2017", "2018", "2019")
 
 # plot predictions by season year
 par(mfrow = c(3,3))
@@ -99,13 +95,8 @@ tactics.df <- do.call(rbind, result) %>%
                                 "1" = "Rare",
                                 "2" = "Sporadic",
                                 "3" = "Seasonal",
-                                "4" = "Habitual"))
-
-# check group counts before generating statistics. Sex is evenly distributed but other variables cannot be compared across groups! 
-tactics.df %>%  group_by(subject_ageClass) %>%
-  summarise(n = n()) 
-
-t <- filter(tactics.df, n > 24*30)
+                                "4" = "Habitual")) %>%
+  filter(n > 500)
 
 ## clust summary - by individual season
 tactics.summary <- tactics.df %>%
@@ -136,7 +127,6 @@ ggplot(t, aes(ag.class.year, prop)) + geom_boxplot(aes(color = ag.class.year)) +
   xlab("Tactic Class") + ylab("Yearly Population Proportion") + labs(color = "Tactic Class")
 
 
-
 ## individual season classifications
 tactics.seasons <- tactics.df %>%
   group_by(ag.class.year) %>%
@@ -148,6 +138,11 @@ tactics.seasons <- tactics.df %>%
   relocate(subject_name, subject_sex, region, site, mean.occupancy, ag.class.aggregate)
 tactics.seasons
 
+# total proportions of tactics (individual-year)
+t <- table(factor(tactics.df$ag.class.year))
+prop.table(t)
+
+
 ## tactic summary over time with rel freqs
 season <- tactics.df %>%
   filter(n > 24*30*2, year.cuts %in% c('2016','2017', '2018', '2019')) %>%
@@ -156,14 +151,14 @@ season <- tactics.df %>%
   mutate(prop=round(n/sum(n), 2)) %>%
   ungroup() %>% mutate(cut.date = ymd(cut.date))
 
-# plot
 ggplot(data = season, aes(x = cut.date, y = prop)) + 
   geom_line(aes(color = as.factor(ag.class.year), linetype = as.factor(ag.class.year))) + 
   geom_point(aes(color = as.factor(ag.class.year))) 
 
-## tactics by sex - compare prop of sexes by ag class
-tactics.sex <- tactics.df %>%
-  group_by(ag.class.year, subject_sex) %>%
+
+## Tactics by sex - compare prop of sexes by ag class
+tactics.sex <- amplitude %>%
+  group_by(tactic.season, subject_sex) %>%
   summarise(n = n()) %>%
   mutate(prop = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,1],
          lwr.ci = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,2],
@@ -171,18 +166,17 @@ tactics.sex <- tactics.df %>%
   )
 tactics.sex
 
-ggplot(tactics.sex, aes(ag.class.year, prop)) + geom_pointrange(
+ggplot(tactics.sex, aes(tactic.season, prop)) + geom_pointrange(
   aes(ymin = lwr.ci, ymax = upr.ci, color = subject_sex), 
     position = position_dodge(0.3)) + xlab("Tactic class") + ylab("Proportion") +
-  labs(color = 'Sex')
+  labs(color = 'Sex') + geom_hline(aes(yintercept = 0.5))
 
 
-## tactics by age class - compare prop of ag classes by age class. Distribution of age classes is not even so hard to compare props between age classes
+## Tactics by age class - compare prop of ag classes by age class. Distribution of age classes is not even so hard to compare props between age classes
 tactics.age <- tactics.df %>%
   mutate(subject_ageClass = recode_factor(as.factor(subject_ageClass), 
-                                          "adult" = "young adult",
-                                          "mature" = "old adult")) %>%
-  group_by(subject_ageClass, ag.class.year) %>%
+                                          "mature adult" = "old adult")) %>%
+  group_by(ag.class.year, subject_ageClass) %>%
   summarise(n = n()) %>%
   mutate(prop = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,1],
          lwr.ci = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,2],
@@ -196,9 +190,22 @@ ggplot(tactics.age, aes(ag.class.year, prop)) + geom_pointrange(
   labs(color = 'Age Class')
 
 
-## tactics by region - frequency calculated relative to the site
+## Tactics by age class and sex
+tactics.age.sex <- tactics.df %>%
+  mutate(subject_ageClass = recode_factor(as.factor(subject_ageClass), 
+                                          "mature adult" = "old adult")) %>%
+  group_by(ag.class.year, subject_ageClass, subject_sex) %>%
+  summarise(n = n()) %>%
+  mutate(prop = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,1],
+         lwr.ci = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,2],
+         upr.ci = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,3]
+  )
+tactics.age.sex
+
+  
+## Tactics by region - frequency calculated relative to the region
 tactics.region <- tactics.df %>%
-  group_by(ag.class.year, region) %>%
+  group_by(region, ag.class.year) %>%
   summarise(n = n()) %>%
   mutate(prop = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,1],
          lwr.ci = MultinomCI(n, conf.level = 0.95, sides = 'two.sided')[,2],
@@ -211,7 +218,8 @@ ggplot(tactics.region, aes(ag.class.year, prop)) + geom_pointrange(
   position = position_dodge(0.3)) + xlab("Tactic class") + ylab("Proportion") +
   labs(color = 'Region')
 
-# site-based results
+
+## Tactics by site - frequency calculated relative to the site
 tactics.site <- tactics.df %>%
   group_by(site, ag.class.year) %>%
   summarise(n = n()) %>%
@@ -222,20 +230,20 @@ tactics.site <- tactics.df %>%
 tactics.site
 
 
-
 ##### Tactics Over Time #####
 # Plot tactics over time for certain individuals
 
 tactics <- do.call(rbind, result) %>%
   mutate(cut.date = ymd(cut.date)) %>% rename(Year = cut.date) %>%
-  filter(subject_name %in% c("Ivy", "Caroline", "Lucy", "Fred"))
+  filter(subject_name %in% c("Fred", "Kegol", "Kiambi", "Harakati"))
 
 
 ggplot(data = tactics, aes(x = Year, y = ag.class.year)) + 
   geom_line(aes(color = as.factor(subject_name), linetype = as.factor(subject_name)), position=position_dodge2(width = 0.4, padding = 2)) + 
   geom_point(aes(color = as.factor(subject_name)), position=position_dodge(width = 0.4))
 
-##### Add seasonal and aggregated results to the dataframe
+
+##### Add seasonal and aggregated results to the dataframe #####
 
 # bind lumped results to movdata
 agg.clust <- clust.result %>%
@@ -250,13 +258,12 @@ clust.df <- do.call(rbind, result) %>%
   as.data.frame()
 
 
-
 ##### Save Tables #####
-# write.csv(tactics.summary, './GMM/results/cluster tables/tactics_summary.csv')
-write.csv(tactics.seasons, './GMM/results/cluster tables/tactics_season_classes_20200722.csv')
-# write.csv(tactics.sex, './GMM/results/cluster tables/tactics_sex.csv')
-#write.csv(tactics.site, './GMM/results/cluster tables/tactics_site.csv')
-# write.csv(tactics.region, './GMM/results/cluster tables/tactics_region.csv')
-# saveRDS(clust.df, './GMM/GMEcollars_002_usedClust_2020-07-14.rds')
+write.csv(tactics.summary, './GMM/results/cluster tables/tactics_summary.csv')
+write.csv(tactics.seasons, './GMM/results/cluster tables/tactics_season_classes_20201027.csv')
+write.csv(tactics.sex, './GMM/results/cluster tables/tactics_sex.csv')
+write.csv(tactics.site, './GMM/results/cluster tables/tactics_site.csv')
+write.csv(tactics.region, './GMM/results/cluster tables/tactics_region.csv')
+saveRDS(clust.df, './GMM/movdata/GMEcollars_003_usedClust_2020-10-30.rds')
 
 
