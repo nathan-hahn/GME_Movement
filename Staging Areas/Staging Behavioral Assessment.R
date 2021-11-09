aa4f2110502fab8a4c6d9cf0113d48936e6ca853
nathan-hahn <nhahnwa@gmail.com>
2021-11-09 13:59:34 -0700

    Added staging length distribution, inter-annual variation in staging frequency by individual. Updated candidate model set to consider mean daily displacement in agriculture.

--- Staging Areas/Staging Behavioral Assessment.R
+++ Staging Areas/Staging Behavioral Assessment.R
@@ -47,6 +47,17 @@ stage.summary <- gme.stage %>%
 
 summary(stage.summary$pct.stage)
 
+#' What is the distribution of staging lengths? Is capped at 12 hrs by parameter space
+
+length <- gme.stage %>%
+  filter(vote == 1) %>%
+  group_by(stage.event.index) %>%
+  tally()
+
+boxplot(length$n)
+
+hist(length$n)
+
 #' Summarise staging frequency distribution by individual and tactic. Use yearly tactics (n=101) 
 #' Note the Rare group with 100% stage percentages have very small numbers of 
 #' stages. See the end of the script for data summary.
@@ -63,6 +74,12 @@ boxplot(stage.summary$pct.stage ~ stage.summary$tactic.season,
         main = 'distribution of staging frequency by individuals in the 4 tactics',
         xlab = 'Tactics (Rare to Habitual)', ylab = 'Pct. of Raids with a Stage')
 
+#' Summarise inter-annual variation in staging by individuals - coefficient of variation
+
+cv.summary <- stage.summary %>%
+  group_by(subject_name) %>%
+  summarise(cv = raster::cv(pct.stage)/100)
+hist(cv.summary$cv, main = 'Individual Inter-Annual Variation in Staging Pct', xlab = 'Coefficient of Variation')  
 
 #' How does the length of the ag bout affect staging frequency? 
 #' 1. Calculate unique id for ag.windows
@@ -105,7 +122,7 @@ window$window.length <- as.numeric(window$window.length)
 # plot and correlation test
 boxplot(window$stage.pct ~ window$window.length)
 
-cor.test(window$stage.pct, window$window.length)
+cor.test(window$stage.pct, window$window.length, method = 'spearman')
 
 
 #' ##### Plot the data on a map
@@ -142,7 +159,7 @@ coordinates(t) <-~x+y
 
 # build MCP's by individual + year
 split <- split(t, list(t$subject_name, t$year.cuts), drop = TRUE)
-mcp <- lapply(split, adehabitatHR::mcp)
+mcp <- lapply(split, adehabitatHR::mcp, percent=95)
 
 # extract areas and attach to movdata
 split.area <- list()
@@ -150,8 +167,12 @@ for(i in 1:length(split)){
   split.area[[i]] <- cbind(split[[i]], newcol = mcp[[i]]$area)
   colnames(split.area[[i]]@data) <- c("subject_name", "year.cuts", "year.mcp.area")
 }
-mcp.areas <- do.call(rbind, split.area)
-gme.stage$year.mcp.area <- mcp.areas@data$year.mcp.area
+mcp.areas <- do.call(rbind, split.area) 
+mcp.areas <- mcp.areas@data %>% 
+  group_by(subject_name, year.cuts) %>%
+  summarise(year.mcp.area = unique(year.mcp.area)) 
+
+gme.stage <- inner_join(gme.stage, mcp.areas, by = c('subject_name', 'year.cuts'))
 
 
 ## Get daily displacement
@@ -159,6 +180,14 @@ gme.stage <- gme.stage %>%
   group_by(dayBurst) %>%
   mutate(dailyDist = sum(dist)) %>% ungroup()
 
+## Get daily displacement only during ag use
+ag.mu.dailyDist <- gme.stage %>%
+  group_by(subject_name, year.cuts, ag.window.ext) %>%
+  summarise(ag.mu.dailyDist = mean(dailyDist)) %>%
+  filter(ag.window.ext == 1) %>%
+  dplyr::select(-ag.window.ext)
+
+gme.stage <- inner_join(gme.stage, ag.mu.dailyDist, by = c('subject_name', 'year.cuts'))
 
 ## Mean bout length by year
 bout.length <- gme.stage %>%
@@ -174,12 +203,22 @@ gme.stage <- inner_join(gme.stage, bout.length, by = c('id', 'year.cuts'))
 
 ## Summarise all data
 mod.df <- gme.stage %>%
-  group_by(subject_name, year.cuts, tactic.season, subject_sex, subject_ageClass, mean.bout, year.mcp.area) %>%
+  group_by(subject_name, year.cuts) %>%
   summarise(
+    # add year-constant variables
+    tactic.season = unique(tactic.season),
+    subject_sex = unique(subject_sex),
+    subject_ageClass = unique(subject_ageClass),
+    mean.bout = unique(mean.bout),
+    year.mcp.area = unique(year.mcp.area),
+    # calc year variables
     mean.ag = mean(ag.used),
-    mu.dailyDist = median(dailyDist),
+    mu.dailyDist = mean(dailyDist),
+    sd.dailyDist = sd(dailyDist),
+    ag.mu.dailyDist = mean(ag.mu.dailyDist),
     n.stage = length(unique(stage.event.index)),
     n.raid = length(unique(ag.day.index)),
+    #n.day = length(unique(dayBurst)),
     pct.stage = n.stage/n.raid,
   ) %>% 
   ungroup()
@@ -207,7 +246,7 @@ summary(mod.1)
 plot(mod.1)
 
 # movement behaviors model
-mod.2 <- lmer(pct.stage ~ log(mu.dailyDist) + log(year.mcp.area) + (1|subject_name), data = mod.df, REML = FALSE)
+mod.2 <- lmer(pct.stage ~ log(mu.dailyDist)+log(ag.mu.dailyDist)+log(year.mcp.area)+(1|subject_name), data = mod.df, REML = FALSE)
 summary(mod.2)
 plot(mod.2)
 
@@ -217,42 +256,47 @@ summary(mod.3)
 plot(mod.3)
 
 # ag use + movement model
-mod.4 <- lmer(pct.stage ~ mean.ag + mean.bout + log(year.mcp.area) + log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
+mod.4 <- lmer(pct.stage ~ mean.ag + mean.bout + log(year.mcp.area) + log(mu.dailyDist) + log(ag.mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
 summary(mod.4)
 
 # ag use + sex/age model
-mod.5 <- lmer(pct.stage ~ mean.ag + mean.bout + subject_sex + subject_ageClass + (1|subject_name), data = mod.df, REML = FALSE)
+#mod.5 <- lmer(pct.stage ~ mean.ag + mean.bout + subject_sex + subject_ageClass + (1|subject_name), data = mod.df, REML = FALSE)
 
 # combo best performers
-mod.5 <- lmer(pct.stage ~ mean.ag + (1|subject_name), data = mod.df)
-mod.6 <- lmer(pct.stage ~ mean.ag + log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
-mod.7 <- lmer(pct.stage ~ mean.ag + log(year.mcp.area) + (1|subject_name), data = mod.df, REML = FALSE)
-mod.8 <- lmer(pct.stage ~ mean.ag + log(year.mcp.area) + log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
-mod.9 <- lmer(pct.stage ~ mean.ag + mean.bout + log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
-mod.10 <- lmer(pct.stage ~ log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
+mod.5 <- lmer(pct.stage ~ mean.ag + subject_sex + subject_ageClass + (1|subject_name), data = mod.df, REML = FALSE)
+mod.6 <- lmer(pct.stage ~ mean.ag + log(year.mcp.area) + log(mu.dailyDist) + log(ag.mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
+mod.7 <- lmer(pct.stage ~ log(mu.dailyDist) + (1|subject_name), data = mod.df, REML = FALSE)
+
 
 library(MuMIn)
 # AICc
-t <- as.data.frame(AICc(mod.global, mod.1, mod.2, mod.3, mod.4, mod.5, mod.6, mod.7, mod.8, mod.9, mod.10))
+t <- as.data.frame(AICc(mod.global, mod.1, mod.2, mod.3, mod.4, mod.5, mod.6, mod.7))
 t$deltaAICc <- qpcR::akaike.weights(t$AICc)$deltaAIC
 t$AICc_weight <- round(qpcR::akaike.weights(t$AICc)$weights, 2)
 t$model_likelihood <- round(qpcR::akaike.weights(t$AICc)$rel.LL, 2)
-t$loglik <- c(logLik(mod.global), logLik(mod.1), logLik(mod.2), logLik(mod.3), logLik(mod.4), logLik(mod.5), logLik(mod.6), logLik(mod.7), logLik(mod.8), logLik(mod.9), logLik(mod.10))
+t$loglik <- c(logLik(mod.global), logLik(mod.1), logLik(mod.2), logLik(mod.3), logLik(mod.4), logLik(mod.5), logLik(mod.6), logLik(mod.7))
 t$mod.formula <- c(mod.global@call$formula, mod.1@call$formula, mod.2@call$formula, mod.3@call$formula, mod.4@call$formula, 
-  mod.5@call$formula, mod.6@call$formula, mod.7@call$formula, mod.8@call$formula, mod.9@call$formula, mod.10@call$formula)
+ mod.5@call$formula, mod.6@call$formula, mod.7@call$formula)
 t <- t[order(t$AICc),]
 t
 
 View(t)
 
+# Final table
+t$mod.formula <- as.character(t$mod.formula)
+write.csv(t, './Staging Areas/stage_behavior_modtable.csv')
 
 ## Deeper eval of mod.6
 
-#' 
-summary(mod.6)
+#'
+plot(mod.2) 
+summary(mod.2)
 
-confint(mod.6)
+confint(mod.2)
+
+sjPlot::tab_model(mod.2)
+sjPlot::plot_model(mod.2)
 
 
-sjPlot::tab_model(mod.6)
-sjPlot::plot_model(mod.6)
+
+
